c098b6d0058b06c6d7d5e42618532e499c5bdc1d
thjxs <tanhongjianx@163.com>
2019-08-30 15:17:53 +0800

    Color

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import color from './color/color'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {color.map(item => {
+        return <span style={{backgroundColor: `rgba(${item.rgb})`, margin: '5px', display: 'inline-block', width: '120px', height: '80px', borderRadius: '25px'}}></span>
+      })}
     </div>
   );
 }
